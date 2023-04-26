024c86e381fab770a93c59ed0fe8a390a22b4133
Breno Juan <breno.juan06@gmail.com>
2023-04-26 10:07:31 -0300

    Finish commit

---
+++ Data/Dtos/UpdateFilmeDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Data.Dtos;
+
+public class UpdateFilmeDto
+{
+
+    [Required(ErrorMessage = "O título do filme é obrigatório")]
+    public string Titulo { get; set; }
+
+
+    [Required(ErrorMessage = "O título do gênero é obrigatório")]
+    [StringLength(30, ErrorMessage = "O tamanho do gênero não pode exceder 30 caracteres")]
+    public string Genero { get; set; }
+
+
+    [Required]
+    [Range(70, 600, ErrorMessage = "A duração deve ter entre 70 e 600 minutos")]
+    public int Duracao { get; set; }
+}
